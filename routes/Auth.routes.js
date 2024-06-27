2a905deb46e203759e0ee589527f82abe6225574
zerotwoadarsh <zerotwoadarsh@gmail.com>
2024-06-27 12:33:56 +0530

    auth-router

--- routes/Auth.routes.js
+++ routes/Auth.routes.js
@@ -3,7 +3,6 @@ const createError = require('http-errors')
 const router = express.Router()
 const User = require('../models/User.model')
 const {authSchema} = require('../helpers/validation_schema')
-const { signAccessToken } = require('../helpers/jwt_helper')
 
 
 router.post('/register', async (req, res, next)=>{
@@ -24,8 +23,9 @@ router.post('/register', async (req, res, next)=>{
         const user = new User(result)
         const savedUser = await user.save()
         const accessToken = await signAccessToken(savedUser.id)
+        const refreshToken = await signRefreshToken(savedUser.id)
 
-        res.send(accessToken)
+        res.send({accessToken, refreshToken})
     }
     catch(error){
         if(error.isJoi === true) {
@@ -47,7 +47,8 @@ router.post('/login', async (req, res, next)=>{
         if(!isMatch) throw createError.Unauthorized("Username/Password not valid")
 
         const accessToken = await signAccessToken(user.id)
-        res.send({accessToken})
+        const refreshToken = await signRefreshToken(user.id)
+        res.send({accessToken, refreshToken})
     }
     catch(error){
         if(error.isJoi === true) return next(createError.BadRequest("Invalid Username/Password"))
@@ -56,7 +57,18 @@ router.post('/login', async (req, res, next)=>{
 })
 
 router.post('/refresh-token', async (req, res, next)=>{
-    res.send("refreshToken")
+    try{
+        const refreshToken = req.body.refreshToken;
+        if(!refreshToken) throw createError.BadRequest()
+
+        const userId = await verifyRefreshToken(refreshToken)
+
+        const accessToken = await signAccessToken(userId)
+        const refToken = await signRefreshToken(userId)
+    res.send({accessToken: accessToken, refreshToken: refToken})
+    }catch(error){
+        next(error)
+    }
 })
 
 router.delete('/logout', async (req, res, next)=>{
